adfa87ee39ce94da79379039b342d3af1774e828
Release Scheduler <release.scheduler@upmesh.io>
2024-01-04 11:56:59 +0700

    ci(2024.104.1156): ✨🐛🚨

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -e
 
-go test -v -coverprofile cover.out
-go tool cover -html cover.out -o cover.html
+CHECKSUM_FILE=./checksum
+
+CHECKSUM_NEW=$(find . -type f -name '*.out' -exec sha256sum {} \; | sort -k 2 | sha256sum | cut -d  ' ' -f1)
+CHECKSUM_OLD=$(cat $CHECKSUM_FILE || true)
+
+if [ "$CHECKSUM_NEW" != "$CHECKSUM_OLD" ];
+then
+    go test -coverprofile cover.out
+    go tool cover -html cover.out -o cover.html
+    find . -type f -name '*.out' -exec sha256sum {} \; | sort -k 2 | sha256sum | cut -d  ' ' -f1 > ./checksum
+fi
