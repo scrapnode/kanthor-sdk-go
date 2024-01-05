dcd1f4e424fdc838572e2d50e95e94fb2a09d857
Release Scheduler <release.scheduler@upmesh.io>
2024-01-05 10:34:00 +0700

    ci(2024.105.1034): ✨🐛🚨

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+export TEST_KANTHOR_SDK_HOST=${TEST_KANTHOR_SDK_HOST:-"localhost:8180"}
+export TEST_KANTHOR_SDK_CREDENTIALS_FILE=${TEST_KANTHOR_SDK_CREDENTIALS_FILE:-"/tmp/workspace.credentials.plain"}
+export TEST_KANTHOR_SDK_APPLICATION_FILE=${TEST_KANTHOR_SDK_APPLICATION_FILE:-"/tmp/application.plain"}
 CHECKSUM_FILE=./checksum
 
-rm -rf *.out
 
+rm -rf *.out
 CHECKSUM_NEW=$(find . -type f -name '*.out' -exec sha256sum {} \; | sort -k 2 | sha256sum | cut -d  ' ' -f1)
 CHECKSUM_OLD=$(cat $CHECKSUM_FILE || true)
 
